995b4b17595be12a6057eeb0ae022d5ffcc5c1eb
ALI075V <alivedaee138@gmail.com>
2025-01-01 14:44:48 +0330

    i create first app

---
+++ Front/React/work/src/App/App.jsx
@@ -0,0 +1,13 @@
+import logo from '../logo.svg';
+import './App.css';
+import Home from '../Home/Home';
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
