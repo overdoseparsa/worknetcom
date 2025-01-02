4d85faa04e42c9129035b005043a79bdd0b1b193
overdoseparsa <parsakhakiy@gmail.com>
2025-01-02 03:23:42 -0500

    create settings from jazadmin

--- WorkNetCom/WorkNetCom/urls.py
+++ WorkNetCom/WorkNetCom/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+def set_language(request):...
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('set_language/' , set_language , name= 'set_language' )
 ]
