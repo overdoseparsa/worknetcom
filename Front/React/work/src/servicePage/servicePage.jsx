57e34c11db74a06a7441c81888a6f62171e24ff4
ALI075V <alivedaee138@gmail.com>
2025-01-25 15:51:05 +0330

    the srvice page is done

--- Front/React/work/src/servicePage/servicePage.jsx
+++ Front/React/work/src/servicePage/servicePage.jsx
@@ -1,11 +1,85 @@
 import React, { Component } from 'react'
 import './servicePage.css'
-import ServiceTage from './serviceTag/serviceTag'
 import ServiceTag from './serviceTag/serviceTag'
+import { Container, Row, Col, Image } from 'react-bootstrap'
+import 'bootstrap'
 export default class Service extends Component {
   render() {
     return (
-      <ServiceTag />
+      <>
+        <Container className='p-0 m-0' fluid>
+          <ServiceTag />
+          <Row className='expalneService'>
+            <Col className='explaneCompany'>
+              <h1>ساختن آینده بر پایه تعالی
+              </h1>
+
+              <Row><Col md={12}>
+                <h1>99%</h1>
+                <h4>
+                  Building Control
+                  Approval Rate</h4>
+              </Col>
+                <Col md={12}>
+                  <h1>320+</h1>
+                  <h4>Active Projects in
+                    Construction Management
+                  </h4>
+                </Col>
+              </Row>
+              <Row><Col md={12}>
+                <h1>200+</h1>
+                <h4>Completed Projects
+                  Every Year</h4>
+              </Col>
+                <Col md={12}>
+                  <h1>4.253</h1>
+                  <h4>Million euros Turnover
+                    in 2024</h4>
+                </Col></Row>
+
+            </Col>
+            <Col className='iamgeContainer p-0'>
+              <Image className='iamgeCompany  p-0' src='image/jyo0shjbauwp-zou0c8r1a.jpg' fluid />
+            </Col>
+          </Row>
+
+          {/*----------------- second part ----------------- */}
+
+
+          <Container className='expalneService2' fluid>
+            <h1 className='headerServicetitle2'> نمونه‌هایی از هنر و تخصص ما </h1>
+            <Row className='rowServiceContainer d-md-flex d-grid'>
+              <Col className='imgserviceContainer col-sm-12 col-md-6 p-0' xs={{ order: 'first' }}>
+                <Image className='imageService' src='image/corporative-building-new-york-city_62972-11.jpg' fluid />
+              </Col>
+              <Col className='textServiceContainer col-sm-12 col-md-6 pt-0 pt-md-5 ' xs={{ order: 'last' }}>
+                <h2 className='mb-4 mt-5'>Lorem ipsum dolor sit</h2>
+                <p>amet consectetur adipisicing elit. Voluptatem aut harum voluptatum accusantium, repudiandae ad reiciendis, quia nemo aperiam excepturi impedit veniam distinctio ut modi ipsa animi provident architecto labore.</p>
+              </Col>
+            </Row>
+            <Row className='rowServiceContainer d-md-flex d-grid'>
+              <Col className='imgserviceContainer col-sm-12 col-md-6 p-0' xs={{ order: 'first' }}>
+                <Image className='imageService' src='image/corporative-building-new-york-city_62972-11.jpg' fluid />
+              </Col>
+              <Col className='textServiceContainer col-sm-12 col-md-6 pt-0 pt-md-5 ' xs={{ order: 'last' }}>
+                <h2 className='mb-4 mt-5'>Lorem ipsum dolor sit</h2>
+                <p>amet consectetur adipisicing elit. Voluptatem aut harum voluptatum accusantium, repudiandae ad reiciendis, quia nemo aperiam excepturi impedit veniam distinctio ut modi ipsa animi provident architecto labore.</p>
+              </Col>
+            </Row>
+            <Row className='rowServiceContainer d-md-flex d-grid'>
+              <Col className='textServiceContainer col-sm-12 col-md-6 pt-0 pt-md-5 ' xs={{ order: 'last' }}>
+                <h2 className='mb-4 mt-5'>Lorem ipsum dolor sit</h2>
+                <p>amet consectetur adipisicing elit. Voluptatem aut harum voluptatum accusantium, repudiandae ad reiciendis, quia nemo aperiam excepturi impedit veniam distinctio ut modi ipsa animi provident architecto labore.</p>
+              </Col>
+              <Col className='imgserviceContainer col-sm-12 col-md-6 p-0' xs={{ order: 'first' }}>
+                <Image className='imageService' src='image/corporative-building-new-york-city_62972-11.jpg' fluid />
+              </Col>
+            </Row>
+          </Container>
+
+        </Container>
+      </>
     )
   }
 }
--- Front/React/work/src/servicePage/serviceTag/serviceTag.jsx
+++ Front/React/work/src/servicePage/serviceTag/serviceTag.jsx
@@ -5,39 +5,51 @@ import { Container, Row, Col, Image } from 'react-bootstrap'
 export default class serviceTag extends Component {
     render() {
         return (
-            <Container className='serContainer'>
+            <Container className='serviceTagContainer' fluid>
                 <h1>خدماتی که تفاوت ایجاد می‌کنند
                 </h1>
-                <Row>
-                    <Col>
-                        <Image >
-
-                        </Image>
+                <Row className='rowService'>
+                    <Col className='serviceInfo'  sm={6}>
+                        <h1 className='numService py-1'>03</h1>
+                        <h3 className='py-1'>برق 
+                        </h3>
+                        <h6>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</h6>
+                        <button className='buttomService' href='#'><h6>اطلاعات بیشتر</h6></button>
                     </Col>
-                    <Col>
-                        <Image >
-
-                        </Image>
+                    <Col className='serviceInfo' sm={6}>
+                        <h1 className='numService py-1'>02</h1>
+                        <h3 className='py-1'>دیتاسنتر
+                        </h3>
+                        <h6>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</h6>
+                        <button className='buttomService' href='#'><h6>اطلاعات بیشتر</h6></button>
                     </Col>
-                    <Col>
-                        <Image >
-
-                        </Image>
+                    <Col className='serviceInfo' sm={6}>
+                        <h1 className='numService py-1'>01</h1>
+                        <h3 className='py-1'><span style={{color: 'rgba(11, 11, 11, 0.59)'}}>(Active&Pasive)</span>شبکه
+                        </h3>
+                        <h6>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</h6>
+                        <button className='buttomService' href='#'><h6>اطلاعات بیشتر</h6></button>
                     </Col>
-                    <Col>
-                        <Image >
-
-                        </Image>
+                    <Col className='serviceInfo' sm={6}>
+                        <h1 className='numService py-1'>06</h1>
+                        <h3 className=' py-1'> طراحی وبسایت 
+                        </h3>
+                        <h6>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</h6>
+                        <button className='buttomService' href='#'><h6>اطلاعات بیشتر</h6></button>
                     </Col>
-                    <Col>
-                        <Image >
-
-                        </Image>
+                    <Col className='serviceInfo' sm={6}>
+                        <h1 className='numService py-1'>05</h1>
+                        <h3 className='py-1'> سیستم امینتی و مداربسته 
+                        </h3>
+                        <h6>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</h6>
+                        <button className='buttomService' href='#'><h6>اطلاعات بیشتر</h6></button>
                     </Col>
-                    <Col>
-                        <Image >
-
-                        </Image>
+                    <Col className='serviceInfo' sm={6}>
+                        <h1 className='numService py-1'>04</h1>
+                        <h3 className='py-1'>UPS 
+                        </h3>
+                        <h6>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</h6>
+                        <button className='buttomService' href='#'><h6>اطلاعات بیشتر</h6></button>
                     </Col>
                 </Row>
             </Container>
